f6699034f402e6f61a8453b468a6a5ed224308b8
sanchyopas <saniagolovanev@gmail.com>
2023-06-27 23:33:30 +0700

    Header line note

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,29 +3,29 @@ import { isMobile, bodyLockToggle } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-// const searchBtn = document.querySelector('.header__search');
-// if (searchBtn) {
-//   let searcBlock = document.querySelector('.search');
-//   searchBtn.addEventListener('click', function (e) {
-//     searcBlock.classList.add('_active');
-//     bodyLockToggle();
-//   })
-// }
+const searchBtn = document.querySelector('.header__search');
+if (searchBtn) {
+  let searcBlock = document.querySelector('.search');
+  searchBtn.addEventListener('click', function (e) {
+    searcBlock.classList.add('_active');
+    bodyLockToggle();
+  })
+}
 
-// const searchInput = document.querySelector('.search__input');
-// if (searchInput) {
-//   searchInput.addEventListener('input', function (e) {
-//     if (e.target.value != '') {
-//       document.querySelector('.search__delete').style.display = 'block';
-//     } else {
-//       document.querySelector('.search__delete').style.display = 'none';
-//     }
-//   });
-// }
+const searchInput = document.querySelector('.search__input');
+if (searchInput) {
+  searchInput.addEventListener('input', function (e) {
+    if (e.target.value != '') {
+      document.querySelector('.search__delete').style.display = 'block';
+    } else {
+      document.querySelector('.search__delete').style.display = 'none';
+    }
+  });
+}
 
-// const clearBtn = document.querySelector('.search__delete');
-// if (clearBtn) {
-//   clearBtn.addEventListener('click', function (e) {
-//     document.querySelector('.search__input').value = '';
-//   })
-// }
+const clearBtn = document.querySelector('.search__delete');
+if (clearBtn) {
+  clearBtn.addEventListener('click', function (e) {
+    document.querySelector('.search__input').value = '';
+  })
+}
